52bea8443d0792b0a21a5b8b53b9f9e2d10a939c
Ruslan Asabidi <ruslanasabidi@gmail.com>
2023-05-11 17:44:25 +0200

    move motion-data.csv to data

--- gather-data.py
+++ gather-data.py
@@ -1,1 +1,25 @@
 # this program gathers sensor data
+
+# TODO: 
+# - DIPPID Daten empfangen
+# - Sinvoll als CSV abspeichern 
+# - Start - Stop zum Abspeichern
+# - pyglet Anwendung
+
+# 3 Aktivitäten: Stehen, Liegen und Winken
+
+# use UPD (via WiFi) for communication
+PORT = 5700
+sensor = SensorUDP(PORT)
+
+# window size
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+sensor.register_callback('accelerometer', handle_accelerometer)
+sensor.register_callback('gyroscope', handle_accelerometer)
+
+# create game window
+window = Window(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+
