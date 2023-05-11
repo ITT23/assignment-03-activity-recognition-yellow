b06ff756f67927b6df64b93754748d91c058d973
Ruslan Asabidi <ruslanasabidi@gmail.com>
2023-05-11 17:53:41 +0200

    get data from DIPPID device
    implemented two callbacks to get the accelorometer and gyroscope data from the DIPPID device

--- gather-data.py
+++ gather-data.py
@@ -8,6 +8,10 @@
 
 # 3 Aktivitäten: Stehen, Liegen und Winken
 
+from pyglet import app, image, clock
+from pyglet.window import Window
+from DIPPID import SensorUDP
+
 # use UPD (via WiFi) for communication
 PORT = 5700
 sensor = SensorUDP(PORT)
@@ -16,10 +20,17 @@ sensor = SensorUDP(PORT)
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 
+def handle_accelerometer(data):
+    print(data)
+
+def handle_gyroscope(data):
+    print(data)
+
 sensor.register_callback('accelerometer', handle_accelerometer)
-sensor.register_callback('gyroscope', handle_accelerometer)
+sensor.register_callback('gyroscope', handle_gyroscope)
+#sensor.register_callback('gyroscope', handle_accelerometer)
 
 # create game window
-window = Window(WINDOW_WIDTH, WINDOW_HEIGHT)
+#window = Window(WINDOW_WIDTH, WINDOW_HEIGHT)
 
 
