24b87485c7698be37f42b15c3c8223d58def489e
Wuehrl-Christoph <christoph-wuehrl@gmx.de>
2023-05-14 20:47:53 +0200

    update activity_recognizer

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn import svm
 from sklearn.preprocessing import scale, StandardScaler, MinMaxScaler
 
+# extracts the frequencies of a given data frame
 def get_frequencies(df):
         frequencies_x = []
         frequencies_y = []
@@ -22,16 +23,19 @@ def get_frequencies(df):
         transformed_data['class'] = df[0].loc[:, 'activity_label']
         return transformed_data
 
+# transforms the data in frequency domain and finds the main frequency
 def get_frequency(data):
     spectrum = np.abs(np.fft.fft(data))
     main_frequency = get_main_frequency(spectrum)
     main_frequency = main_frequency[0][-1]
     return main_frequency
 
+# returns the position of the dominant frequency
 def get_main_frequency(spectrum):
     highest_frequency = max(spectrum)
     return np.where(spectrum == highest_frequency)
 
+# normalizes the data for training
 def pre_process_data(data):
     global scaler
     scaler = MinMaxScaler()
@@ -41,6 +45,7 @@ def pre_process_data(data):
     data_scaled[['acc_x', 'acc_y', 'acc_z']] = scaled_sample
     return data_scaled
 
+# trains the SVM after normalizing the data
 def train_model(jumping_frequencies, shaking_frequencies, lying_frequencies):
     frames = pd.concat([jumping_frequencies, shaking_frequencies, lying_frequencies], ignore_index=True)
     normalized_frames =  pre_process_data(frames)
@@ -58,7 +63,7 @@ class Recognizer():
         self.waving_data = []
         self.classifier = None
         
-
+    # reads all files named jumping, waving or lying and trains the classifier with them
     def train_classifier(self):
         
         files = os.listdir('data')
@@ -78,27 +83,32 @@ class Recognizer():
             if(len(re.findall('lying', file)) != 0):
                 self.lying_data.append(pd.read_csv(f'data/{file}'))
 
-        try:
-            jumping_frequencies =  get_frequencies(self.jumping_data)
-            waving_frequencies =  get_frequencies(self.waving_data)
-            lying_frequencies = get_frequencies(self.lying_data)
-            classifier =  train_model(jumping_frequencies, waving_frequencies, lying_frequencies)
-            self.classifier = classifier
-        except:
-            print('data folder is empty')
-        
+        # transforms the data in frequency domain
+        jumping_frequencies =  get_frequencies(self.jumping_data)
+        waving_frequencies =  get_frequencies(self.waving_data)
+        lying_frequencies = get_frequencies(self.lying_data)
+
+        # trains the classifier
+        classifier =  train_model(jumping_frequencies, waving_frequencies, lying_frequencies)
+        self.classifier = classifier
+    
+    # classifies the new data and returns a prediction
     def classify(self, data_x, data_y, data_z):
+        # finds the dominant frequency for all three axis
         x = get_frequency(data_x)
         y = get_frequency(data_y)
         z = get_frequency(data_z)
 
+        # transforms the data in a dataframe
         data_set = [x, y, z]
         columns = ['acc_x', 'acc_y', 'acc_z']
         dataframe = pd.DataFrame(data_set, index=columns)
         dataframe = dataframe.T
 
+        # normalizes the data
         scaled = scaler.transform(dataframe[['acc_x', 'acc_y', 'acc_z']])
         
+        # predicts the new motion
         predicted_motion = self.classifier.predict(scaled)
 
         return predicted_motion[0]
