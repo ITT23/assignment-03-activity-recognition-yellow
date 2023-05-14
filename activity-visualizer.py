3ae45287192a34d792ed7f034db9a5aa9ce3bbc7
Ruslan Asabidi <ruslanasabidi@gmail.com>
2023-05-14 11:05:45 +0200

    recognizer class

--- activity-visualizer.py
+++ activity-visualizer.py
@@ -1,4 +1,95 @@
 # this program visualizes activities with pyglet
 
-import activity-recognizer as activity
+#import activity-recognizer as activity
+#import pyglet
+# this program recognizes activities
+
+import pandas as pd
+import numpy as np
+import os
+import re
+# install scikit-learn
+from sklearn import svm
 import pyglet
+from DIPPID import SensorUDP
+from threading import Thread
+from time import sleep
+from dippid_data_handler import Data_Handler
+from sklearn.preprocessing import scale, StandardScaler, MinMaxScaler
+from pyglet.window import Window
+from pyglet import app, image, clock
+
+from activity_recognizer import Recognizer
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+INPUT_TEXT = 'Press 1 button to start recognition'
+JUMPING_TEXT = 'you are jumping'
+SHAKING_TEXT = 'you are shaking the phone'
+
+PORT = 5700
+sensor = SensorUDP(PORT)
+
+jumping_data = []
+shaking_data = []
+lying_data = []
+
+scaler = None
+
+data_handler = Data_Handler()
+recognizer = Recognizer()
+
+recognizer.train_classifier()
+
+
+label = None
+predicted_motion = ""
+
+# game window
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+window = Window(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+def handle_accelerometer(data):
+    data_handler.update_accelorometer_data(data)
+
+sensor.register_callback('accelerometer', handle_accelerometer)
+
+@window.event
+def on_draw():
+
+    window.clear()
+    
+
+def wait():
+    sleep(3)
+
+def gather_data(dt):
+    global predicted_motion
+
+    time_thread = Thread(target=wait, daemon=True)
+    time_thread.start()
+    data_x = []
+    data_y = []
+    data_z = []
+
+    while(time_thread.is_alive()):
+        data_x.append(data_handler.get_accelorometer_value('x'))
+        data_y.append(data_handler.get_accelorometer_value('y'))
+        data_z.append(data_handler.get_accelorometer_value('z'))
+        sleep(0.02)
+        
+    predicted_motion = recognizer.classify(data_x, data_y, data_z)
+    print(predicted_motion)
+
+
+
+
+# latency of input sound
+clock.schedule_interval(gather_data, 3.1)
+
+# run game
+app.run()
+
+
+#main()
---
+++ activity_recognizer.py
@@ -0,0 +1,102 @@
+import os
+import pandas as pd
+import re
+import numpy as np
+# install scikit-learn
+from sklearn import svm
+from sklearn.preprocessing import scale, StandardScaler, MinMaxScaler
+
+def get_frequencies(df):
+        frequencies_x = []
+        frequencies_y = []
+        frequencies_z = []
+        for data in df:
+            frequencies_x.append(get_frequency(data.loc[:,'acc_x']))
+            frequencies_y.append(get_frequency(data.loc[:, 'acc_y']))
+            frequencies_z.append(get_frequency(data.loc[:, 'acc_z']))
+
+        combined_data = [frequencies_x, frequencies_y, frequencies_z]
+        columns = ['acc_x', 'acc_y', 'acc_z']
+        transformed_data = pd.DataFrame(combined_data, index=columns)
+        transformed_data = transformed_data.T
+        transformed_data['class'] = df[0].loc[:, 'activity_label']
+        return transformed_data
+
+def get_frequency(data):
+    spectrum = np.abs(np.fft.fft(data))
+    main_frequency = get_main_frequency(spectrum)
+    main_frequency = main_frequency[0][0]
+    return main_frequency
+
+def get_main_frequency(spectrum):
+    highest_frequency = max(spectrum)
+    return np.where(spectrum == highest_frequency)
+
+def pre_process_data(data):
+    global scaler
+    scaler = MinMaxScaler()
+    scaler.fit(data[['acc_x', 'acc_y', 'acc_z']])
+    scaled_sample = scaler.transform(data[['acc_x', 'acc_y', 'acc_z']])
+    data_scaled = data.copy()
+    data_scaled[['acc_x', 'acc_y', 'acc_z']] = scaled_sample
+    return data_scaled
+
+def train_model(jumping_frequencies, shaking_frequencies, lying_frequencies):
+    frames = pd.concat([jumping_frequencies, shaking_frequencies, lying_frequencies], ignore_index=True)
+    normalized_frames =  pre_process_data(frames)
+    classifier = svm.SVC(kernel='linear')
+    classifier.fit(normalized_frames[['acc_x', 'acc_y', 'acc_z']], normalized_frames['class'])
+    return classifier
+
+
+
+class Recognizer():
+
+    def __init__(self):
+        self.jumping_data = []
+        self.lying_data = []
+        self.waving_data = []
+        self.classifier = None
+        
+
+    def train_classifier(self):
+        files = os.listdir('data')
+
+        # get jumping data
+        for file in files:
+            if(len(re.findall('jumping', file)) != 0):
+                self.jumping_data.append(pd.read_csv(f'data/{file}'))
+
+        # get waving data
+        for file in files:
+            if(len(re.findall('waving', file)) != 0):
+                self.waving_data.append(pd.read_csv(f'data/{file}'))
+
+        # get lying data
+        for file in files:
+            if(len(re.findall('lying', file)) != 0):
+                self.lying_data.append(pd.read_csv(f'data/{file}'))
+
+        jumping_frequencies =  get_frequencies(self.jumping_data)
+        waving_frequencies =  get_frequencies(self.waving_data)
+        lying_frequencies = get_frequencies(self.lying_data)
+        classifier =  train_model(jumping_frequencies=jumping_frequencies, shaking_frequencies=waving_frequencies, lying_frequencies=lying_frequencies)
+        self.classifier = classifier
+    
+    def classify(self, data_x, data_y, data_z):
+        x = get_frequency(data_x)
+        y = get_frequency(data_y)
+        z = get_frequency(data_z)
+
+        data_set = [x, y, z]
+        columns = ['acc_x', 'acc_y', 'acc_z']
+        dataframe = pd.DataFrame(data_set, index=columns)
+        dataframe = dataframe.T
+
+        scaled = scaler.transform(dataframe[['acc_x', 'acc_y', 'acc_z']])
+        
+        predicted_motion = self.classifier.predict(scaled)
+
+        return predicted_motion[0]
+
+    
