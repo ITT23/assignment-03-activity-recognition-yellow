b1dde900c2fbbc69ed3f6e637b70202432e281d0
Wuehrl-Christoph <christoph-wuehrl@gmx.de>
2023-05-14 20:55:14 +0200

    reorganize file

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -5,55 +5,6 @@ import numpy as np
 from sklearn import svm
 from sklearn.preprocessing import scale, StandardScaler, MinMaxScaler
 
-# extracts the frequencies of a given data frame
-def get_frequencies(df):
-        frequencies_x = []
-        frequencies_y = []
-        frequencies_z = []
-        for data in df:
-            frequencies_x.append(get_frequency(data.loc[:,'acc_x']))
-            frequencies_y.append(get_frequency(data.loc[:, 'acc_y']))
-            frequencies_z.append(get_frequency(data.loc[:, 'acc_z']))
-
-        combined_data = [frequencies_x, frequencies_y, frequencies_z]
-        columns = ['acc_x', 'acc_y', 'acc_z']
-        transformed_data = pd.DataFrame(combined_data, index=columns)
-        transformed_data = transformed_data.T
-        transformed_data['class'] = df[0].loc[:, 'activity_label']
-        return transformed_data
-
-# transforms the data in frequency domain and finds the main frequency
-def get_frequency(data):
-    spectrum = np.abs(np.fft.fft(data))
-    main_frequency = get_main_frequency(spectrum)
-    main_frequency = main_frequency[0][-1]
-    return main_frequency
-
-# returns the position of the dominant frequency
-def get_main_frequency(spectrum):
-    highest_frequency = max(spectrum)
-    return np.where(spectrum == highest_frequency)
-
-# normalizes the data for training
-def pre_process_data(data):
-    global scaler
-    scaler = MinMaxScaler()
-    scaler.fit(data[['acc_x', 'acc_y', 'acc_z']])
-    scaled_sample = scaler.transform(data[['acc_x', 'acc_y', 'acc_z']])
-    data_scaled = data.copy()
-    data_scaled[['acc_x', 'acc_y', 'acc_z']] = scaled_sample
-    return data_scaled
-
-# trains the SVM after normalizing the data
-def train_model(jumping_frequencies, shaking_frequencies, lying_frequencies):
-    frames = pd.concat([jumping_frequencies, shaking_frequencies, lying_frequencies], ignore_index=True)
-    normalized_frames =  pre_process_data(frames)
-    classifier = svm.SVC(kernel='rbf')
-    classifier.fit(normalized_frames[['acc_x', 'acc_y', 'acc_z']], normalized_frames['class'])
-    return classifier
-
-
-
 class Recognizer():
 
     def __init__(self):
@@ -111,5 +62,50 @@ class Recognizer():
         predicted_motion = self.classifier.predict(scaled)
 
         return predicted_motion[0]
+  
+# extracts the frequencies of a given data frame
+def get_frequencies(df):
+        frequencies_x = []
+        frequencies_y = []
+        frequencies_z = []
+        for data in df:
+            frequencies_x.append(get_frequency(data.loc[:,'acc_x']))
+            frequencies_y.append(get_frequency(data.loc[:, 'acc_y']))
+            frequencies_z.append(get_frequency(data.loc[:, 'acc_z']))
 
-    
+        combined_data = [frequencies_x, frequencies_y, frequencies_z]
+        columns = ['acc_x', 'acc_y', 'acc_z']
+        transformed_data = pd.DataFrame(combined_data, index=columns)
+        transformed_data = transformed_data.T
+        transformed_data['class'] = df[0].loc[:, 'activity_label']
+        return transformed_data
+
+# transforms the data in frequency domain and finds the main frequency
+def get_frequency(data):
+    spectrum = np.abs(np.fft.fft(data))
+    main_frequency = get_main_frequency(spectrum)
+    main_frequency = main_frequency[0][-1]
+    return main_frequency
+
+# returns the position of the dominant frequency
+def get_main_frequency(spectrum):
+    highest_frequency = max(spectrum)
+    return np.where(spectrum == highest_frequency)
+
+# normalizes the data for training
+def pre_process_data(data):
+    global scaler
+    scaler = MinMaxScaler()
+    scaler.fit(data[['acc_x', 'acc_y', 'acc_z']])
+    scaled_sample = scaler.transform(data[['acc_x', 'acc_y', 'acc_z']])
+    data_scaled = data.copy()
+    data_scaled[['acc_x', 'acc_y', 'acc_z']] = scaled_sample
+    return data_scaled
+
+# trains the SVM after normalizing the data
+def train_model(jumping_frequencies, shaking_frequencies, lying_frequencies):
+    frames = pd.concat([jumping_frequencies, shaking_frequencies, lying_frequencies], ignore_index=True)
+    normalized_frames =  pre_process_data(frames)
+    classifier = svm.SVC(kernel='rbf')
+    classifier.fit(normalized_frames[['acc_x', 'acc_y', 'acc_z']], normalized_frames['class'])
+    return classifier
